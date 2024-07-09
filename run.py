b7ac98f3905e82a160591e53fa5e320c23916800
Harshal Bondre <91895030+harshalbondre101@users.noreply.github.com>
2024-07-09 23:51:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app.main import create_app
 app, socket_io = create_app()
 
 if __name__ == "__main__":
-    socket_io.run(app, allow_unsafe_werkzeug=True,debug=True, use_reloader=True)
+    socket_io.run(app)
